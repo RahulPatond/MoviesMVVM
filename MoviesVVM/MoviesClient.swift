0cfb5b9c461b7c6beabe9617782f676f91a593ab
Rahul Patond <rahulpatond@Axcels-MacBook-Pro.local>
2017-06-30 20:55:25 +0530

    first commit

---
+++ MoviesVVM/MoviesClient.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesClient.swift
+//  MoviesVVM
+//
+//  Created by Rahul Patond on 29/06/17.
+//  Copyright © 2017 Rahul Patond. All rights reserved.
+//
+
+import UIKit
+
+class MoviesClient: NSObject {
+
+}
---
+++ MoviesVVM/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  MoviesVVM
+//
+//  Created by Rahul Patond on 29/06/17.
+//  Copyright © 2017 Rahul Patond. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}
